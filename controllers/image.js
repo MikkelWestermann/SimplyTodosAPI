114dca5bed7f78ff9d9edf78df3731f17ca7b159
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2018-12-06 22:09:28 +0100

    Add Heroku Database

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: process.env.CLARIFAI_API_KEY
+ apiKey: '6fb0f5d251d04395894b2b8b6bb40c97'
 });
 
 const handleApicall = (req, res) => {
